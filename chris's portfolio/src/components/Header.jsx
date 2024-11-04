2bf415b806e0ee7fa1a850c89e237b4cd0d769df
cjsquared-dev <camjohnson0618@gmail.com>
2024-11-03 21:12:16 -0700

    setup componentfile structure and build basic components to render raw app

---
+++ chris's portfolio/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1>Chris's Portfolio</h1>
+        </header>
+    )
+};
+
+export default Header;
---
+++ chris's portfolio/src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav className="navigation">
+        <ul>
+            <li>
+            <a href="#about">About Me</a>
+            </li>
+            <li>
+            <a href="#projects">Projects</a>
+            </li>
+            <li>
+            <a href="#contact">Contact</a>
+            </li>
+        </ul>
+        </nav>
+    );
+    }
+
+export default Navigation;
